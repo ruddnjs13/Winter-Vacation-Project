66dbb869d75c5bb304340299b410edca862b2845
ruddnjs13 <ruddnjs013@gmail.com>
2025-01-21 23:35:41 +0900

    InputSetting

--- Assets/Settings/Input/InputReaderSO.cs
+++ Assets/Settings/Input/InputReaderSO.cs
@@ -1,7 +1,34 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 [CreateAssetMenu(fileName = "InputReader", menuName = "SO/InputReader")]
-public class InputReaderSO : ScriptableObject
+public class InputReaderSO : ScriptableObject, Controls.IPlayerActions
 {
-    //private Controls
+    public event Action JumpKeyPressed;
+    
+    private Controls _controls;
+
+    public Vector3 InputDirection {get; private set;}
+
+    private void OnEnable()
+    {
+        if (_controls == null)
+        {
+            _controls = new Controls();
+            _controls.Player.SetCallbacks(this);
+        }
+        _controls.Enable();
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        InputDirection = context.ReadValue<Vector3>();
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if(context.performed)
+            JumpKeyPressed?.Invoke();
+    }
 }
